6624dbcf6667f6d3b2d39470c2ad31374f772a30
Keming <believe.75467@gmail.com>
2015-04-06 15:59:03 +0800

    安裝 PostgreSQL 建立 Model -> User, Issue

--- app/settings.py
+++ app/settings.py
@@ -64,10 +64,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'HeyServer',
+        'USER': 'HeyServerAdmin',
+        'PASSWORD': 'Ok',
+        'HOST': '127.0.0.1',
+        'POST': ''
+    }
 }
 
 # Internationalization
---
+++ core/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class UserModel(models.Model):
+    user_id = models.BigIntegerField(unique=True, blank=False, null=False, primary_key=True)
+    access_token = models.CharField(max_length=50, default='', blank=False, unique=True)
+
+    student_id = models.CharField(max_length=50, default='', blank=False, unique=True)
+
+    good = models.PositiveIntegerField(default=0, blank=False, null=False)
+    bad = models.PositiveIntegerField(default=0, blank=False, null=False)
+
+    class Meta:
+        db_table = "user"
+
+
+class IssueModel(models.Model):
+    user = models.ForeignKey(UserModel)
+
+    actor = models.CharField(max_length=50, blank=False, null=False)
+    event = models.CharField(max_length=50, blank=False, null=False)
+    place = models.CharField(max_length=50, blank=True, null=False)
+
+    latitude = models.FloatField(null=True, blank=False)
+    longitude = models.FloatField(null=True, blank=False)
+
+    good = models.PositiveIntegerField(default=0, blank=False, null=False)
+    bad = models.PositiveIntegerField(default=0, blank=False, null=False)
+
+    class Meta:
+        db_table = "issue"
