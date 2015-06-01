82f389c9f780fd15ed4f062462dbe5d4e79c9d7e
KemingChen <believe75467@gmail.com>
2015-06-01 23:19:20 +0800

    finish sort recommend list

--- core/recommend/recommend_manager.py
+++ core/recommend/recommend_manager.py
@@ -28,28 +28,41 @@ class RecommendManager():
 
     def _add_subject(self, issue):
         self._add(self.subjects, issue.subject_key, issue.subject)
-        self.descriptions[issue.subject_key] = {}
-        self.places[issue.subject_key] = {}
 
     def _add_description(self, issue):
-        self._add(self.descriptions[issue.subject_key], issue.description_key, issue.description)
+        self._add(self.descriptions, issue.description_key, issue.description)
+
+        subject = self.subjects[issue.subject_key]
+        if "descriptions" not in subject:
+            subject["descriptions"] = dict()
+        subject["descriptions"][issue.description_key] = self.descriptions[issue.description_key]
 
     def _add_place(self, issue):
-        self._add(self.places[issue.subject_key], issue.place_key, issue.place)
+        self._add(self.places, issue.place_key, issue.place)
+
+        subject = self.subjects[issue.subject_key]
+        if "places" not in subject:
+            subject["places"] = dict()
+        subject["places"][issue.place_key] = self.places[issue.place_key]
 
     def get_commend_list(self):
+        subjects = list(reversed(sorted(self.subjects.items(), key=lambda s: s[1]["count"])))
+
         recommends = dict()
-        recommends["subjects"] = dict()
-        for key, subject in self.subjects.iteritems():
+        recommend_subjects = recommends["subjects"] = dict()
+        for key, subject in subjects:
             subject_content = subject["content"]
-            recommends["subjects"][subject_content] = {
+            recommend_subjects[subject_content] = {
                 "descriptions": list(),
                 "places": list(),
             }
-            for none, description in self.descriptions[key].iteritems():
-                if description["content"] is not None:
-                    recommends["subjects"][subject_content]["descriptions"].append(description["content"])
-            for none, place in self.places[key].iteritems():
-                if place["content"] is not None:
-                    recommends["subjects"][subject_content]["places"].append(place["content"])
+
+            descriptions = list(reversed(sorted(subject["descriptions"].items(), key=lambda s: s[1]["count"])))
+            for description in descriptions:
+                recommend_subjects[subject_content]["descriptions"].append(description[1]["content"])
+
+            places = list(reversed(sorted(subject["places"].items(), key=lambda s: s[1]["count"])))
+            for place in places:
+                recommend_subjects[subject_content]["places"].append(place[1]["content"])
+
         return recommends
